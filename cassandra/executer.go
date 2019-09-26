0043cc515c7baf08237ac343a4133a0fbc3ad882
Jack Murdock <kcajmagic@gmail.com>
2019-09-26 09:50:28 -0700

    adding comment about spaces

--- cassandra/executer.go
+++ cassandra/executer.go
@@ -135,7 +135,8 @@ func (b *dbDecorator) insert(records []db.Record) (int, error) {
 		if ttl > 2147483647 {
 			ttl = 2147483647
 		}
-		batch.Query("INSERT INTO devices.events  (device_id,type,birthdate,deathdate,data,nonce,alg,kid) VALUES (?,?,?,?,?,?,?,?) USING TTL ?;",
+		// there can be no spaces for some weird reason. Otherwise the database returns and error.
+		batch.Query("INSERT INTO devices.events (device_id,type,birthdate,deathdate,data,nonce,alg,kid) VALUES (?,?,?,?,?,?,?,?) USING TTL ?;",
 			record.DeviceID,
 			record.Type,
 			record.BirthDate,
